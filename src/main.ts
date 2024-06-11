23f864818d560f7ee2f525f340c169196876ac55
Achilleas Katsaros <a.katsaros1@aade.gr>
2024-06-11 15:38:18 +0300

    chore: third part of conversion

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,32 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
 import { environment } from './environments/environment';
+import { AppComponent } from './app/app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ToastrModule } from 'ngx-toastr';
+import { TableModule } from 'primeng/table';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app/app-routing.module';
+import { CalendarModule } from 'primeng/calendar';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, ModalModule.forRoot(), CalendarModule, AppRoutingModule, BsDropdownModule.forRoot(), FormsModule, ReactiveFormsModule, TableModule, ToastrModule.forRoot(), FontAwesomeModule),
+        { provide: LocationStrategy, useClass: HashLocationStrategy },
+        provideAnimations(),
+        provideHttpClient(withInterceptorsFromDi())
+    ]
+})
   .catch(err => console.error(err));
